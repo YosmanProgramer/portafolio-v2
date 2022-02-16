e68277d4755f0777eea2a6168eb53ea29751afa1
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2022-02-16 12:01:30 -0500

    add new portafolios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,86 +1,90 @@
-'use strict';
+'use strict'
 
 // Elementos
-const header = document.querySelector('.header');
-const nav = document.querySelector('.nav');
-const nav__list = document.querySelector('.nav__list');
-const sections = document.querySelectorAll('.section');
-const sectionAbout = document.querySelector('#about');
-const btnUp = document.querySelector('.btn--up');
-const menuBtn = document.querySelector('.nav__icon');
+const header = document.querySelector('.header')
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.nav__list')
+const sections = document.querySelectorAll('.section')
+const btnUp = document.querySelector('.btn--up')
+const menuBtn = document.querySelector('.nav__icon')
 
 // Page navigation
-nav__list.addEventListener('click', function (e) {
-  e.preventDefault();
+navList.addEventListener('click', function (e) {
+  e.preventDefault()
   if (e.target.classList.contains('nav__link')) {
-    const nav__link = e.target.getAttribute('href');
+    const navLink = e.target.getAttribute('href')
     // Scroll smooth behavior
-    document.querySelector(nav__link).scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(navLink).scrollIntoView({ behavior: 'smooth' })
   }
-});
+})
 
 // Sticky navigation
 const headerObserver = new IntersectionObserver(
   entries => {
-    const [entrie] = entries;
-    if (!entrie.isIntersecting) nav.classList.add('nav--sticky');
-    else nav.classList.remove('nav--sticky');
+    const [entrie] = entries
+    if (!entrie.isIntersecting) nav.classList.add('nav--sticky')
+    else nav.classList.remove('nav--sticky')
   },
   {
     root: null,
-    threshold: 1,
+    threshold: 1
   }
-);
-headerObserver.observe(header);
+)
+headerObserver.observe(header)
 
 // btn up view
 const btnObserver = new IntersectionObserver(
   entries => {
-    const [entrie] = entries;
-    if (!entrie.isIntersecting) btnUp.classList.add('btn--active');
-    else btnUp.classList.remove('btn--active');
+    const [entrie] = entries
+    if (!entrie.isIntersecting) btnUp.classList.add('btn--active')
+    else btnUp.classList.remove('btn--active')
   },
   {
     root: null,
-    threshold: 0,
+    threshold: 0
   }
-);
-btnObserver.observe(header);
+)
+btnObserver.observe(header)
 
 // btn up click to header
 btnUp.addEventListener('click', function (e) {
-  e.preventDefault();
-  document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
-});
+  e.preventDefault()
+  document.querySelector('header').scrollIntoView({ behavior: 'smooth' })
+})
 
 // Menu button
 menuBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  menuBtn.classList.toggle('nav__icon--active');
-  nav__list.classList.toggle('nav__list--active');
-});
+  e.preventDefault()
+  menuBtn.classList.toggle('nav__icon--active')
+  navList.classList.toggle('nav__list--active')
+})
 
 // nav list close menu
-nav__list.addEventListener('click', function (e) {
-  e.preventDefault();
+navList.addEventListener('click', function (e) {
+  e.preventDefault()
   if (e.target.classList.contains('nav__link')) {
-    menuBtn.classList.remove('nav__icon--active');
-    nav__list.classList.remove('nav__list--active');
+    menuBtn.classList.remove('nav__icon--active')
+    navList.classList.remove('nav__list--active')
   }
-});
+})
 
 // sections animations
 const sectionObserver = new IntersectionObserver(
   function (entries, observer) {
-    const [entry] = entries;
-    if (!entry.isIntersecting) return;
-    entry.target.classList.remove('section--hidden');
-    observer.unobserve(entry.target);
+    const [entry] = entries
+    if (!entry.isIntersecting) return
+    entry.target.classList.remove('section--hidden')
+    observer.unobserve(entry.target)
   },
   { root: null, threshold: 0.15 }
-);
+)
 
 sections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
+})
+
+window.addEventListener('scroll', function () {
+  menuBtn.classList.remove('nav__icon--active')
+  navList.classList.remove('nav__list--active')
+})
--- assets/js/utils/swiper.js
+++ assets/js/utils/swiper.js
@@ -1,44 +1,45 @@
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination, Thumbs } from 'swiper';
+import Swiper, { Navigation, Pagination, Thumbs } from 'swiper'
 
 // configure Swiper to use modules
-Swiper.use([Navigation, Pagination, Thumbs]);
+Swiper.use([Navigation, Pagination, Thumbs])
 
-const swiper_thumbs = new Swiper(".swiper-thumbs", {
-    spaceBetween: 10,
-    slidesPerView: 2,
-    freeMode: true,
-    watchSlidesProgress: true,
-    breakpoints: {
-        768: {
-            slidesPerView: 3,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
-        1366: {
-            slidesPerView: 5
-        }
+const swiperThumbs = new Swiper('.swiper-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 2,
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 4
+    },
+    1366: {
+      slidesPerView: 5
     }
-});
+  }
+})
 
-const swiper_main = new Swiper('.swiper-main', {
-    // Optional parameters
-    loop: true,
+// eslint-disable-next-line no-unused-vars
+const swiperMain = new Swiper('.swiper-main', {
+  // Optional parameters
+  loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 
-    thumbs: {
-        swiper: swiper_thumbs,
-    },
-});
+  thumbs: {
+    swiper: swiperThumbs
+  }
+})
